dce3e5ad0bf2964906c9b6ae293b23ac58102d87
Fulano2141 <macuto2142dt@gmail.com>
2021-02-03 17:20:30 -0400

    keep alive components

--- src/views/ComponensAdvanced.vue
+++ src/views/ComponensAdvanced.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    |
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+
+    <!-- Not Vue -->
+    <!-- <manage-goals v-if="selectedCom === 'manage-goals'"></manage-goals>
+    <active-goals v-if="selectedCom === 'active-goals'"></active-goals> -->
+    <!--  -->
+
+    <keep-alive>
+      <component :is="selectedCom"></component>
+    </keep-alive>
+    <hr />
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -10,7 +23,8 @@
     <courser-goal>
       <template #default="slotProps">
         <h1>{{ slotProps.item }}</h1>
-        <p>{{ slotProps["another-prop"] }}</p> <!-- Doesn't work :C -->
+        <p>{{ slotProps["another-prop"] }}</p>
+        <!-- Doesn't work :C -->
         <p>{{ slotProps.anotherProp }}</p>
       </template>
     </courser-goal>
@@ -22,16 +36,31 @@ import TheHeader from "../components/problem-two/TheHeader.vue";
 import BadgeList from "../components/problem-two/BadgeList.vue";
 import UserInfo from "../components/problem-two/UserInfo.vue";
 import CourserGoal from "../components/problem-two/CourserGoal.vue";
+import ManageGoals from "../components/problem-two/ManageGoals.vue";
+import ActiveGoals from "../components/problem-two/ActiveGoals.vue";
 export default {
-  components: { TheHeader, BadgeList, UserInfo, CourserGoal },
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourserGoal,
+    ManageGoals,
+    ActiveGoals,
+  },
   data() {
     return {
+      selectedCom: "manage-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedCom = cmp;
+    },
   },
 };
 </script>
