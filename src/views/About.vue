74d77cb8845dc3296bdda8af75491007f5f345c9
Fulano2141 <macuto2142dt@gmail.com>
2021-02-07 14:18:45 -0400

    about page with images

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-for="(image, index) in images" :key="index">
+      <img :src="image" heigth="700px" widgth="700px" />
+      <hr />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    images: [
+      require("../assets/Animation.jpg"),
+      require("../assets/Lifecycle.jpg"),
+      require("../assets/Vuex_0.jpg"),
+      require("../assets/Vuex_1.jpg"),
+      require("../assets/Vuex_2.jpg"),
+    ],
+  }),
+};
 </script>
 
 <style>
